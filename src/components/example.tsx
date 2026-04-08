27cd4fe252bba1aaa22e221462fb9a6744a72de3
Mason Williams <mason190607@gmail.com>
2026-04-07 19:05:16 -0700

    Applied feedback

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,20 +1,20 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-hohc-blue-500 w-2/3 flex-col items-center gap-1 justify-self-center rounded-xl p-8">
-      <div className="bg-hohc-yellow-100 mb-5 w-full p-5 text-center">
+    <div className="bg-hohc-blue-500 w-2/3 flex flex-col items-center gap-1 justify-self-center rounded-xl p-8">
+      <div className="bg-hohc-yellow-100 mb-5 w-1/2 p-5 text-center">
         {props.text1}
       </div>
 
-      <div className="flex w-full flex-row gap-1 text-center">
-        <div className="bg-hohc-blue-100 w-1/2 rounded-full p-5">
+      <div className="flex w-1/2 flex-row gap-1 text-center">
+        <div className="bg-hohc-blue-100 w-full rounded-full p-5">
           {props.text2}
         </div>
-        <div className="bg-hohc-blue-100 w-1/2 rounded-full p-5">
+        <div className="bg-hohc-blue-100 w-full rounded-full p-5">
           {props.text2}
         </div>
       </div>
 
-      <div className="bg-hohc-blue-700 mt-5 p-5 text-center text-white">
+      <div className="bg-hohc-blue-700 w-1/2 mt-5 p-5 text-center text-white">
         {props.text3}
       </div>
     </div>
