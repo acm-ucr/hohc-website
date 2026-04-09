28af2a091f8f97a687adc4fe2a581f12ab76a3cb
Benito <benitosan14@gmail.com>
2026-04-09 10:20:47 -0700

    Create practice component

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,15 +1,21 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-hohc-blue-500 flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-hohc-blue-100 w-1/2 p-5 text-left">{props.text1}</div>
+    <div className="bg-hohc-blue-500 flex w-2/3 flex-col items-center gap-4 justify-self-center rounded-xl p-8">
+      <div className="w-1/2 bg-white p-5 text-center text-lg">
+        {props.text1}
+      </div>
 
       <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-hohc-blue-100 w-full p-5 text-left">
+        <div className="bg-hohc-blue-100 w-full rounded-full p-5 text-center text-lg">
+          {props.text2}
+        </div>
+        <div className="bg-hohc-blue-100 w-full rounded-full p-5 text-center text-lg">
           {props.text2}
         </div>
       </div>
-
-      <div className="bg-hohc-blue-100 w-1/2 p-5">{props.text3}</div>
+      <div className="bg-hohc-blue-700 w-1/2 p-5 text-center text-lg text-white">
+        {props.text3}
+      </div>
     </div>
   );
 };
