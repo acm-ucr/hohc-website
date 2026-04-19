a51bf28e334cf683aac551a824cd1c49535a8748
Benito <benitosan14@gmail.com>
2026-04-19 14:26:24 -0700

    Update with needed changes

--- src/components/home/social.tsx
+++ src/components/home/social.tsx
@@ -1,21 +1,21 @@
 import Image, { StaticImageData } from "next/image";
 
-interface Props {
+interface SocialProps {
   title: string;
   link: string;
   icon: StaticImageData;
 }
 
-const Social = ({ title, link, icon }: Props) => {
+const Social = ({ title, link, icon }: SocialProps) => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row items-center justify-center gap-2">
       <div className="flex items-center justify-center self-center rounded-full">
-        <Image src={icon} alt={title} width={50} height={50} />
+        <Image src={icon} alt={title} />
       </div>
 
-      <div className="border-hohc-blue-500 flex flex-col rounded-lg">
-        <div className="pl-1 font-semibold">{title}</div>
-        <div className="pl-2 text-sm underline">{link}</div>
+      <div className="border-hohc-blue-500 flex flex-col gap-3 rounded-lg">
+        <div className="pl-1 text-4xl font-medium">{title}</div>
+        <div className="pl-2 text-3xl underline">{link}</div>
       </div>
     </div>
   );
