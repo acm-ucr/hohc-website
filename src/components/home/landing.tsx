809bebb162e99597fb31d3791d9f1982e1d91b4d
Ronald Fan <rfan018@ucr.edu>
2026-04-20 19:10:30 -0700

    Font corrected

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -10,8 +10,8 @@ const Landing = () => {
           alt="HOHC Landing Image"
           className="h-auto w-full object-cover"
         />
-        <div className="absolute inset-0 h-1/2 bg-gradient-to-b from-white to-transparent"></div>
-        <p className="text-hohc-blue-700 absolute top-24 text-center text-8xl font-bold whitespace-nowrap drop-shadow-[2px_4px_2px_rgba(255,255,255,0.8)]">
+        <div className="absolute inset-0 h-1/2 bg-gradient-to-b from-white to-transparent" />
+        <p className="text-hohc-blue-700 font-hohc-kanit absolute top-24 text-center text-8xl font-bold whitespace-nowrap drop-shadow-[2px_4px_2px_rgba(255,255,255,0.8)]">
           Hands on Health Care
         </p>
       </div>
