1f31af536ed2c38a76a559b2d46bcbcc416eab05
milkrs <245736871+milkrs@users.noreply.github.com>
2026-04-20 21:14:34 -0700

    resolve issues

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,21 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 interface HeaderProps {
-  icon1: string;
-  icon2: string;
+  icon1: StaticImageData;
+  icon2: StaticImageData;
   title: string;
   subtitle?: string;
 }
 
 const Header = ({ icon1, icon2, title, subtitle }: HeaderProps) => {
-  const textSize = subtitle ? "text-6xl" : "text-7xl";
+  const textSize = subtitle ? "text-6xl mt-4 " : "text-7xl";
   return (
-    <div className="flex items-center p-8">
+    <div className="flex items-center justify-center gap-3 p-8">
       <Image src={icon1} alt={title} className="size-30" />
 
-      <div className="flex flex-col items-center gap-1">
-        <p
-          className={`${textSize} text-hohc-blue-600 font-hohc-kanit font-bold`}
-        >
-          {title}
-        </p>
-        <p className="text-hohc-blue-400 font-hohc-kanit text-4xl">
-          {subtitle}
-        </p>
+      <div className="font-hohc-kanit flex flex-col items-center gap-2">
+        <p className={`${textSize} text-hohc-blue-600 font-bold`}>{title}</p>
+        <p className="text-hohc-blue-400 text-4xl">{subtitle}</p>
       </div>
 
       <Image src={icon2} alt={title} className="size-30" />
