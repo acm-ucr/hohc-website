4437622a3fc18a50a88446729b6213382749427a
Larry Zhou <zhoularry15@gmail.com>
2026-04-22 02:16:26 -0700

    added prettier

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,44 +4,29 @@ import Star from "@/public/Star Icon.svg";
 
 const NotFound = () => {
   return (
-    <div className="relative h-screen w-full bg-hohc-yellow-100">     
-      <div className=" absolute left-130 top-10 h-full w-full scale-150 rotate-4 opacity-5">
-        <Image 
-          src={Star} 
-          alt="" 
-          fill 
-          className="object-contain" 
-          priority 
-        />
+    <div className="bg-hohc-yellow-100 relative h-screen w-full">
+      <div className="absolute top-10 left-130 h-full w-full scale-150 rotate-4 opacity-5">
+        <Image src={Star} alt="" fill className="object-contain" priority />
       </div>
 
-      <div className="relative z-10 flex flex-col translate-y-1 items-center">
-        <p className="text-huge font-hohc-kanit top-20">
-          404
+      <div className="relative z-10 flex translate-y-1 flex-col items-center">
+        <p className="text-huge font-hohc-kanit top-20">404</p>
+        <p className="font-hohc-kanit mb-5 translate-x-5 text-7xl">
+          Oops! The page you were <br className="mb-5" />
+          looking for doesn't exist.
         </p>
-        <p className="text-7xl translate-x-5 font-hohc-kanit mb-5">
-          Oops! The page you were <br className="mb-5"/>looking for doesn't exist.
-        </p>
-        <Link href="/" className="mt-10 bg-hohc-blue-500 px-20 py-10 text-white text-4xl rounded-4xl shadow-lg hover:shadow-2xl hover:-translate-y-1 transition-all">
+        <Link
+          href="/"
+          className="bg-hohc-blue-500 mt-10 rounded-4xl px-20 py-10 text-4xl text-white shadow-lg transition-all hover:-translate-y-1 hover:shadow-2xl"
+        >
           Back to Home
-          </Link>
+        </Link>
       </div>
-      
-      
 
-
-      <div className=" absolute right-120 -bottom-60 h-full w-full scale-150 -rotate-20 opacity-5 ">
-        <Image 
-          src={Star} 
-          alt="" 
-          fill 
-          className="object-contain" 
-          priority 
-        />
+      <div className="absolute right-120 -bottom-60 h-full w-full scale-150 -rotate-20 opacity-5">
+        <Image src={Star} alt="" fill className="object-contain" priority />
       </div>
-      
     </div>
-    
   );
 };
 
