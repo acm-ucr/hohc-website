9e31b13cf9c2cbf78c901db86d148b64f4834d05
Larry Zhou <zhoularry15@gmail.com>
2026-04-23 17:07:43 -0700

    completed all of the comments (1)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
-import Star from "@/public/Star Icon.svg";
+import Star from "@/public/star-icon.svg";
 
 const NotFound = () => {
   return (
-    <div className="bg-hohc-yellow-100 relative h-screen w-full">
+    <div className="bg-hohc-yellow-200 relative h-screen w-full overflow-hidden">
       <div className="absolute top-10 left-130 h-full w-full scale-150 rotate-4 opacity-5">
         <Image src={Star} alt="" fill className="object-contain" priority />
       </div>
 
-      <div className="relative z-10 flex translate-y-1 flex-col items-center">
-        <p className="text-huge font-hohc-kanit top-20">404</p>
-        <p className="font-hohc-kanit mb-5 translate-x-5 text-7xl">
-          Oops! The page you were <br className="mb-5" />
-          looking for doesn't exist.
+      <div className="relative z-10 mt-6 flex translate-x-5 translate-y-1 flex-col items-center">
+        <p className="text-huge font-hohc-kanit leading-tight">404</p>
+        <p className="font-hohc-kanit mb-5 w-3/5 text-center text-7xl leading-normal">
+          Oops! The page you were looking for doesn't exist.
         </p>
         <Link
           href="/"
-          className="bg-hohc-blue-500 mt-10 rounded-4xl px-20 py-10 text-4xl text-white shadow-lg transition-all hover:-translate-y-1 hover:shadow-2xl"
+          className="bg-hohc-blue-500 mt-4 rounded-4xl px-20 py-10 text-4xl text-white shadow-lg transition-all hover:-translate-y-1 hover:shadow-2xl"
         >
           Back to Home
         </Link>
