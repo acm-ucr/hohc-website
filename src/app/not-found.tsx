b7cc1130aadd7ed8a2edb18703b81a418616cd1a
Larry Zhou <zhoularry15@gmail.com>
2026-04-24 09:19:21 -0700

    changed comments (2)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
-import Star from "@/public/star-icon.svg";
+import Star1 from "@/public/star-icon.svg";
+import Star2 from "@/public/star-icon.svg";
 
 const NotFound = () => {
   return (
     <div className="bg-hohc-yellow-200 relative h-screen w-full overflow-hidden">
-      <div className="absolute top-10 left-130 h-full w-full scale-150 rotate-4 opacity-5">
-        <Image src={Star} alt="" fill className="object-contain" priority />
+      <div className="absolute right-50 bottom-100">
+        <Image
+          src={Star1}
+          alt=""
+          className="h-full w-full scale-850 rotate-4 object-contain opacity-5"
+        />
       </div>
 
       <div className="relative z-10 mt-6 flex translate-x-5 translate-y-1 flex-col items-center">
@@ -22,8 +27,12 @@ const NotFound = () => {
         </Link>
       </div>
 
-      <div className="absolute right-120 -bottom-60 h-full w-full scale-150 -rotate-20 opacity-5">
-        <Image src={Star} alt="" fill className="object-contain" priority />
+      <div className="absolute bottom-15 left-50">
+        <Image
+          src={Star2}
+          alt=""
+          className="h-full w-full scale-850 -rotate-20 object-contain opacity-5"
+        />
       </div>
     </div>
   );
