6b4267e219b7d3dbb43cd4b63027a109efefd821
Mason Williams <mason190607@gmail.com>
2026-04-30 14:31:41 -0700

    Fixed formatting

--- src/components/home/socials.tsx
+++ src/components/home/socials.tsx
@@ -2,7 +2,7 @@ import { socialsLinks } from "@/data/socials";
 import Social from "./social";
 
 const Socials = () => (
-  <div>
+  <div className="flex flex-col items-start justify-center gap-10">
     {socialsLinks.map(({ link, title, icon }, index) => (
       <Social key={index} title={title} link={link} icon={icon} />
     ))}
