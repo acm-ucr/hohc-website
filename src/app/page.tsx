9d6c91c4c9bd581b1223473c85cca0e479726f63
Benito <benitosan14@gmail.com>
2026-05-01 14:33:39 -0700

    Make position changes to Stars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Landing from "../components/home/landing";
 import Mission from "@/components/home/mission";
-import Header from "@/components/header";
-import CrossIcon from "@/public/cross-icon.svg";
-import PillIcon from "@/public/pill-icon.svg";
 import AboutUs from "@/components/aboutus";
 
 const Home = () => {
   return (
     <div>
       <Landing />
-      <Header icon1={CrossIcon} icon2={PillIcon} title="Our Mission" />
       <Mission />
       <AboutUs />
     </div>
--- src/components/home/mission.tsx
+++ src/components/home/mission.tsx
@@ -1,28 +1,33 @@
 import StarBackground from "@/public/star-background.svg";
 import StarBackground2 from "@/public/star-background-2.svg";
 import Image from "next/image";
+import Header from "@/components/header";
+import CrossIcon from "@/public/cross-icon.svg";
+import PillIcon from "@/public/pill-icon.svg";
 
 const Mission = () => {
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center py-16">
+    <div className="flex h-100 flex-col">
+      <Header icon1={CrossIcon} icon2={PillIcon} title="Our Mission" />
+
+      <div className="relative">
         <Image
           src={StarBackground}
           alt="Star Background"
-          className="absolute w-90 opacity-80"
+          className="-translate-x--1/3 absolute top-0 left-0 w-[520px] -translate-y-1/4 opacity-80"
         />
-
-        <div className="flex flex-col items-center p-2 px-8 py-4 text-center text-2xl font-semibold">
+        <Image
+          src={StarBackground2}
+          alt="Star Background"
+          className="absolute top-0 right-0 w-[520px] translate-y-1/3"
+        />
+      </div>
+      <div className="flex items-center justify-center py-16">
+        <div className="flex w-6/7 flex-col items-center p-2 px-8 py-4 text-center text-4xl font-semibold">
           To connect UCR undergraduate students interested in the healthcare
           field to various hands-on opportunities and help them explore
           different careers within healthcare!
         </div>
-
-        <Image
-          src={StarBackground2}
-          alt="Star Background"
-          className="absolute right-0 w-90"
-        />
       </div>
     </div>
   );
