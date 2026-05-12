4f4d1fc79fa2787adc32207b5b4e19920953b697
Ronald Fan <rfan018@ucr.edu>
2026-05-12 16:13:49 -0700

    breakpoint for drop shadow

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -11,7 +11,7 @@ const Landing = () => {
           className="h-auto w-full object-cover"
         />
         <div className="absolute inset-0 h-1/2 bg-gradient-to-b from-white to-transparent" />
-        <p className="text-hohc-blue-700 font-hohc-kanit absolute top-10 text-center text-2xl font-bold whitespace-nowrap drop-shadow-[2px_4px_2px_rgba(255,255,255,0.8)] sm:top-16 sm:text-4xl md:top-20 md:text-6xl xl:top-24 xl:text-8xl">
+        <p className="text-hohc-blue-700 font-hohc-kanit absolute top-10 text-center text-2xl font-bold whitespace-nowrap drop-shadow-[1px_2px_1px_rgba(255,255,255,0.8)] sm:top-16 sm:text-4xl md:top-20 md:text-6xl md:drop-shadow-[2px_4px_2px_rgba(255,255,255,0.8)] xl:top-24 xl:text-8xl">
           Hands on Health Care
         </p>
       </div>
