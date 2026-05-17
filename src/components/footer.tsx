41b19e7e41c9c1c4c2a1d154e56759be9ab85b75
Benito <benitosan14@gmail.com>
2026-05-16 20:50:43 -0700

    Add responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,29 +4,31 @@ import { FaInstagram } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="bg-hohc-blue-700 border-hohc-blue-600 relative border-t-[6px] px-5 py-3">
-      <div className="flex items-center gap-3">
+    <div className="bg-hohc-blue-700 border-hohc-blue-600 relative border-t-[6px] px-5 py-3 sm:px-5 md:px-8">
+      <div className="flex items-center gap-3 sm:gap-1 md:gap-2">
         <Link href="/">
           <Image
             src="/Logo.svg"
             alt="HOHC Logo"
             width={32}
             height={32}
-            className="h-full w-full"
+            className="h-full w-full sm:h-12 sm:w-12 md:h-13 md:w-13"
             priority
           />
         </Link>
-        <span className="text-3xl text-white">HOHC</span>
+        <span className="text-2xl text-white sm:text-3xl">HOHC</span>
       </div>
-      <p className="text-lg text-white">Hands on Health Care</p>
-      <p className="text-lg text-white">At UCR</p>
+      <p className="text-sm text-white sm:text-base md:text-lg">
+        Hands on Health Care
+      </p>
+      <p className="text-sm text-white sm:text-base md:text-lg">At UCR</p>
 
       <div className="absolute right-5 bottom-3">
         <Link
           href={"https://www.instagram.com/handsonhealthcare_ucr"}
           target="_blank"
         >
-          <FaInstagram className="h-14 w-14 text-white" />
+          <FaInstagram className="h-10 w-10 text-white sm:h-12 sm:w-12 md:h-14 md:w-14" />
         </Link>
       </div>
     </div>
