79dc2cec4ab2a571888417c646eed68af8598f5d
Larry Zhou <zhoularry15@gmail.com>
2026-05-16 19:29:24 -0700

    completed 404 not found responsiveness

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,32 +6,67 @@ import Star2 from "@/public/star-icon.svg";
 const NotFound = () => {
   return (
     <div className="bg-hohc-yellow-200 relative h-screen w-full overflow-hidden">
-      <div className="absolute right-50 bottom-100">
+      <div className="absolute bottom-30 left-10 md:right-50 md:bottom-80 md:left-auto">
         <Image
           src={Star1}
-          alt=""
-          className="h-full w-full scale-850 rotate-4 object-contain opacity-5"
+          alt="Star"
+          className="h-full w-full scale-200 -rotate-25 object-contain opacity-5 transition-transform md:scale-800 md:rotate-4"
         />
       </div>
-
-      <div className="relative z-10 mt-6 flex translate-x-5 translate-y-1 flex-col items-center">
-        <p className="text-huge font-hohc-kanit leading-tight">404</p>
-        <p className="font-hohc-kanit mb-5 w-3/5 text-center text-7xl leading-normal">
+      <div className="absolute -top-10 right-30 md:hidden">
+        <Image
+          src={Star2}
+          alt="Star"
+          className="h-full w-full scale-200 -rotate-3 object-contain opacity-5 transition-transform"
+        />
+      </div>
+      <div className="absolute top-20 -right-20 md:hidden">
+        <Image
+          src={Star1}
+          alt="Star"
+          className="h-full w-full scale-200 rotate-7 object-contain opacity-5 transition-transform"
+        />
+      </div>
+      <div className="absolute top-35 -left-10 md:hidden">
+        <Image
+          src={Star2}
+          alt="Star"
+          className="h-full w-full scale-200 rotate-50 object-contain opacity-5 transition-transform"
+        />
+      </div>
+      <div className="absolute top-1/2 -right-10 md:hidden">
+        <Image
+          src={Star1}
+          alt="Star"
+          className="h-full w-full scale-200 -rotate-7 object-contain opacity-5 transition-transform"
+        />
+      </div>
+      <div className="absolute -bottom-15 -left-1 md:hidden">
+        <Image
+          src={Star2}
+          alt="Star"
+          className="h-full w-full scale-200 -rotate-5 object-contain opacity-5 transition-transform"
+        />
+      </div>
+      <div className="relative z-10 flex h-full flex-col items-center justify-center px-4 md:mt-6 md:h-auto md:translate-x-5 md:translate-y-1 md:justify-start md:px-0">
+        <p className="md:text-huge font-hohc-kanit text-center text-8xl leading-tight">
+          404
+        </p>
+        <p className="font-hohc-kanit mb-5 w-11/12 text-center text-4xl leading-normal md:w-3/5 md:text-7xl">
           Oops! The page you were looking for doesn't exist.
         </p>
         <Link
           href="/"
-          className="bg-hohc-blue-500 mt-4 rounded-4xl px-20 py-10 text-4xl text-white shadow-lg transition-all hover:-translate-y-1 hover:shadow-2xl"
+          className="bg-hohc-blue-500 mt-4 rounded-2xl px-12 py-3 text-center text-xl text-white shadow-lg transition-all hover:-translate-y-1 hover:shadow-2xl md:rounded-4xl md:px-20 md:py-10 md:text-4xl"
         >
           Back to Home
         </Link>
       </div>
-
-      <div className="absolute bottom-15 left-50">
+      <div className="absolute -right-3 bottom-3 md:right-auto md:bottom-15 md:left-50">
         <Image
           src={Star2}
-          alt=""
-          className="h-full w-full scale-850 -rotate-20 object-contain opacity-5"
+          alt="Star"
+          className="h-full w-full scale-200 -rotate-5 object-contain opacity-5 transition-transform md:scale-800 md:-rotate-20"
         />
       </div>
     </div>
