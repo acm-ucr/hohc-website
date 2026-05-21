c98a0f7e733b3f46834a85506e6aacdd1c3565a5
Ronald Fan <rfan018@ucr.edu>
2026-05-20 20:10:53 -0700

    address comments

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 
 const Header = ({ icon1, icon2, title, subtitle }: HeaderProps) => {
   const textSize = subtitle
-    ? "text-2xl sm:text-4xl md:text-6xl mt-4 "
+    ? "text-2xl sm:text-4xl md:text-6xl mt-2 sm:mt-3 md:mt-4 "
     : "text-3xl sm:text-5xl md:text-7xl";
   return (
     <div className="flex items-center justify-center gap-1 p-4 sm:gap-2 sm:p-6 md:gap-3 md:p-8">
@@ -20,8 +20,8 @@ const Header = ({ icon1, icon2, title, subtitle }: HeaderProps) => {
       />
 
       <div className="font-hohc-kanit flex flex-col items-center gap-1 md:gap-2">
-        <p className={`${textSize} text-hohc-blue-600 font-bold`}>{title}</p>
-        <p className={`text-hohc-blue-400 ${textSize}`}>{subtitle}</p>
+        <p className={`${textSize} text-hohc-blue-600 text-center font-bold`}>{title}</p>
+        <p className={`text-hohc-blue-400 text-center text-xl sm:text-2xl md:text-4xl`}>{subtitle}</p>
       </div>
 
       <Image
