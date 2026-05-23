b8660c6d91e9079960203f7c776c67b122f77b20
Yilin Liu <yilinliu741@gmail.com>
2026-05-23 16:10:30 -0700

    Formatted + upcoming events array

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,12 +2,12 @@ import Header from "@/components/header";
 import HeartIcon from "@/public/heart-icon.svg";
 import CrossIcon from "@/public/cross-icon.svg";
 
-import CalendarDemo from "@/components/events/calendarCall";
+import FullCalendar from "@/components/events/calendarCall";
 const Page = () => {
   return (
     <div>
       <Header icon1={HeartIcon} icon2={CrossIcon} title="Event Calendar" />
-      <CalendarDemo />
+      <FullCalendar />
     </div>
   );
 };
