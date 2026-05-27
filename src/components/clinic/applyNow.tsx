ead92b853791ab55d2c76ebf43daf9417baa0c67
Larry Zhou <zhoularry15@gmail.com>
2026-05-26 23:22:16 -0700

    made changes to Apply Now

--- src/components/clinic/applyNow.tsx
+++ src/components/clinic/applyNow.tsx
@@ -4,13 +4,13 @@ import Background from "@/public/background.svg";
 
 const ApplyNow = () => {
   return (
-    <div className="relative h-screen w-full overflow-hidden">
+    <div className="relative w-full overflow-hidden pb-20 md:pb-24 lg:pb-32">
       <Image
         src={Background}
         alt=""
-        className="absolute -bottom-9 w-full md:bottom-10 lg:top-0"
+        className="absolute -bottom-9 h-auto w-full md:-bottom-40 lg:top-0"
       />
-      <div className="relative z-10 mt-56 flex translate-y-68 flex-col items-center px-4 md:mt-48 lg:mt-6">
+      <div className="relative z-10 mt-16 flex flex-col items-center px-4 md:mt-24 lg:mt-32">
         <p className="font-hohc-kanit text-hohc-blue-800 mb-2 text-center text-3xl font-black md:mb-12 md:text-5xl lg:text-6xl">
           Apply Now
         </p>
