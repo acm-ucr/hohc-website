d63d3e8b914b11bf424f9e2678cef1cbe15391d8
Larry Zhou <zhoularry15@gmail.com>
2026-05-28 07:52:54 -0700

    changed margin sizes

--- src/components/clinic/applyNow.tsx
+++ src/components/clinic/applyNow.tsx
@@ -4,22 +4,22 @@ import Background from "@/public/background.svg";
 
 const ApplyNow = () => {
   return (
-    <div className="relative w-full overflow-hidden pb-13 md:pb-18 lg:pb-32">
+    <div className="relative w-full overflow-hidden pb-8 md:pb-12 lg:pb-32">
       <Image
         src={Background}
         alt=""
-        className="absolute -bottom-9 h-auto w-full md:-bottom-40 lg:top-0"
+        className="absolute -bottom-8 -bottom-9 h-auto w-full md:-bottom-40 lg:top-0"
       />
       <div className="relative z-10 mt-16 flex flex-col items-center px-4 md:mt-24 lg:mt-32">
-        <p className="font-hohc-kanit text-hohc-blue-800 mb-2 text-center text-3xl font-black md:mb-12 md:text-5xl lg:text-6xl">
+        <p className="font-hohc-kanit text-hohc-blue-800 mb-2 text-center text-3xl font-black md:mb-6 md:text-5xl lg:mb-12 lg:text-6xl">
           Apply Now
         </p>
-        <p className="font-hohc-kanit mb-2 w-48 text-center text-base leading-normal md:mb-5 md:w-80 md:text-xl lg:h-1/2 lg:w-1/3 lg:text-3xl">
+        <p className="font-hohc-kanit mb-2 w-48 text-center text-base leading-normal md:mb-3 md:w-80 md:text-xl lg:mb-5 lg:h-1/2 lg:w-1/3 lg:text-3xl">
           apply instructions here apply instructions here
         </p>
         <Link
           href="/clinics"
-          className="bg-hohc-blue-800 font-hohc-kanit mt-2 w-fit rounded-xl px-6 py-3 text-center text-xl text-white shadow-lg transition-all hover:-translate-y-1 hover:shadow-2xl md:mt-4 md:rounded-3xl md:px-10 md:py-5 md:text-3xl lg:rounded-4xl lg:px-14 lg:py-7 lg:text-4xl"
+          className="bg-hohc-blue-800 font-hohc-kanit mt-2 w-fit rounded-xl px-6 py-3 text-center text-xl text-white shadow-lg transition-all hover:-translate-y-1 hover:shadow-2xl md:mt-3 md:rounded-3xl md:px-10 md:py-5 md:text-3xl lg:mt-4 lg:rounded-4xl lg:px-14 lg:py-7 lg:text-4xl"
         >
           Apply Here
         </Link>
